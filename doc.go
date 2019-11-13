23b503d2c0528b281559ef998201515a765fc7af
Giovanni Spera <giovanni.spera2011@libero.it>
2019-11-13 14:41:21 +0100

    Updated doc with functions and constants

--- doc.go
+++ doc.go
@@ -5,4 +5,17 @@
 //	e.SetVar("x", 42)
 //For evaluating expressions use the Eval method
 //	e.Eval("x+2")
+//
+// Functions
+//
+//eval comes with some builtins functions, others may be added in future:
+//Ln, Log10, Log2, Sin, Cos, Tan, Sqrt
+//	e.Eval("ln(2)")
+//
+// Math Constants
+//
+//eval comes with some predefine math constants, others may be added in future:
+//Pi, E
+//
+//	e.Eval("pi")
 package eval
